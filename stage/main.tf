dc0fb83dff56715693cf4143c17d6925a70ab044
Ashok <ashokkumarrsp@yahoo.com>
2019-06-22 17:13:12 +0100

    remote state changes - s3 bucket in aws

--- stage/main.tf
+++ stage/main.tf
@@ -3,8 +3,45 @@ provider "aws" {
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
 }
+
+terraform {
+backend "s3" {
+bucket = "tf-bucket-vetrikootani"
+dynamodb_table= "terraform-state-lock-dynamo"
+region = "us-east-1"
+key = "terraform.tfstate"
+encrypt = "true"
+}
+}
 module "frontend" {
   source = "../modules/frontend-app"
+}
+resource "aws_s3_bucket" "remotestate" {
+  bucket = "tf-bucket-vetrikootani"
+  acl    = "private"
+
+  tags = {
+    Name        = "tf state bucket vetrikootani"
+    Environment = "Dev"
+  }
+  versioning {
+    enabled = true
+  }
+}
+
+# create a dynamodb table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+name = "terraform-state-lock-dynamo"
+hash_key = "LockID"
+read_capacity = 20
+write_capacity = 20
+attribute {
+name = "LockID"
+type = "S"
+}
+tags = {
+Name = "DynamoDB Terraform State Lock Table"
+}
 }
 resource "aws_instance" "example" {
   ami = "ami-07b4156579ea1d7ba"
